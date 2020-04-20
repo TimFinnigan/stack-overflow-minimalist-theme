aaeaafe18db0cc8a6a4557d2d59740397eabb793
Tim Finnigan <tim.finnigan1@gmail.com>
2020-04-19 23:06:00 -0400

    add vote threshold check

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // document.querySelector(".js-filter-btn a:nth-child(3)").click();
 // document.getElementsByClassName(".answers-subheader")[0].style.display = 'none';
+
+// Hide answers below threshold
+let votes = document.getElementsByClassName("js-vote-count");
+
+// Skip first element because it is the question
+for (let i = 1; i < votes.length; i++) {
+  let element = votes[i];
+  let voteCount = parseInt(element.getAttribute("data-value"));
+  if (voteCount < 2) {
+    let num = i + 2;
+    let result = document.querySelector(".answer:nth-of-type(" + num + ")");
+    if (result) {
+      result.style.display = "none";
+    }
+  }
+}
