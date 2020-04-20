91cf94af8bf904f8f5ebd4c18ff20f3580246d2c
Tim Finnigan <tim.finnigan1@gmail.com>
2020-04-19 22:10:21 -0400

    update portions to hide

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-document.querySelector(".js-filter-btn a:nth-child(3)").click();
+// document.querySelector(".js-filter-btn a:nth-child(3)").click();
+// document.getElementsByClassName(".answers-subheader")[0].style.display = 'none';
